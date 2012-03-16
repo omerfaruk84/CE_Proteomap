3ed510d7aace9f408608a587d2b15acba34d80ae
Sencer Selcuk <sselcuk@princeton.edu>
2012-03-16 18:07:52 -0400

    hata bildirimi genisletildi

--- js/fonksiyonlar.js
+++ js/fonksiyonlar.js
@@ -5,11 +5,11 @@ function guncelle(){
 	chrome.browserAction.setBadgeBackgroundColor({color:[190, 190, 190, 230]});
 	chrome.browserAction.setBadgeText({text:"x"});	
 	chrome.browserAction.setTitle({"title": "Not translatable!"});
-	document.getElementById('hata').textContent='{"error": {"translator": "untranslated", "adres": \"' + url + '\"}}';
 	//acik sitenin urlsi aliniyor, urlReg'den hangi translatorun
 	//kullanilacagina karar verilip sayfaya o translator yukleniyor
 	chrome.tabs.getSelected(null, function(tab) {
 		var i,url=tab.url;
+		document.getElementById('hata').textContent='{"error": {"translator": "untranslated", "adres": \"' + url + '\"}}';
 		document.getElementById('varUrl').textContent=url;
 		for(i=0;i<urlReg.length;i++){
 			if (url.match(urlReg[i][0])){ 
